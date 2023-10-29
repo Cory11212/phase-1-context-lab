e0ae15ccd1547a387a263c50cabf1958cfa50b9d
Cory <corymclean0@gmail.com>
2023-10-29 15:36:02 -0400

    call

--- index.js
+++ index.js
@@ -1,4 +1,63 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    const employeeRecord = {}
+    employeeRecord.firstName = array[0]
+    employeeRecord.familyName = array[1]
+    employeeRecord.title = array[2]
+    employeeRecord.payPerHour = array[3]
+    employeeRecord.timeInEvents = []
+    employeeRecord.timeOutEvents = []
+    return employeeRecord
+}
+
+function createEmployeeRecords(array){
+    const records = []
+    for(const element of array){
+        records.push(createEmployeeRecord(element))
+    }
+    return records
+}
+
+function createTimeInEvent(date){
+    const timeEvent = {}
+    timeEvent.type = "TimeIn"
+    timeEvent.hour = parseInt(date.slice(11))
+    timeEvent.date = date.slice(0, 10)
+    this.timeInEvents.push(timeEvent)
+    return this
+}
+
+function createTimeOutEvent(date){
+    const timeEvent = {}
+    timeEvent.type = "TimeOut"
+    timeEvent.hour = parseInt(date.slice(11))
+    timeEvent.date = date.slice(0, 10)
+    this.timeOutEvents.push(timeEvent)
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    let num1 = 0
+    let num2 = 0
+    this.timeInEvents.forEach(element => {
+        if(element.date === date){
+             num1 = element.hour/100
+        }
+    });
+
+    this.timeOutEvents.forEach(element => {
+        if(element.date === date){
+             num2 = element.hour/100
+        }
+    });
+
+    return num2 - num1
+}
+
+function wagesEarnedOnDate(date){
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +80,19 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(array, firstNameString){
+    for(const element of array){
+        if(element.firstName === firstNameString){
+            return element
+        }
+    }
+}
+
+function calculatePayroll(array){
+    let allPay = []
+    for(const element of array){
+        allPay.push(allWagesFor.call(element))
+    }
+    return allPay.reduce((accumulator, currentValue) => accumulator + currentValue)
+
+}
